4787ae33553875eb53d0f49275000095da151fa7
Amber Taveras <taveras.amber@gmail.com>
2023-05-04 19:43:16 -0400

    outlines about page

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+function About () {
+    return (
+        <div>
+            <h1>Here is my bio</h1>
+            <h1>Here are my tech skills</h1>
+            <h1>Here are the links to my github and linkedin</h1>
+            <h1>Here are my featured projects</h1>
+        </div>
+    );
+}
+
+
+export default About;
