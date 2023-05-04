ca94fde6f03f8b42ca0953218973aebf81eb2c29
Amber Taveras <taveras.amber@gmail.com>
2023-05-04 19:31:29 -0400

    renders nav bar with list of page names

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+function Navigation () {
+    return (
+        <nav>
+            <ul>
+                <li>Projects</li>
+                <li>Resume</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    );
+}
+
+
+export default Navigation;
