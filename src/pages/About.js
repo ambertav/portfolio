7abcbd64578c375b88934888f44f9a93bcbd5591
Amber Taveras <taveras.amber@gmail.com>
2023-05-06 15:20:36 -0400

    uses state to hold bio length, renders bio and passes state

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+
+import Bio from '../components/Bio';
+
 function About () {
+    const [ bioLength, setBioLength ] = useState('short');
+
     return (
         <div>
-            <h1>Here is my bio</h1>
+            <div>
+                Set Bio Length
+                    <ul>
+                        <li>Short</li>
+                        <li>Medium</li>
+                        <li>Long</li>
+                    </ul>
+            </div>
+            <Bio bioLength={bioLength} />
             <h1>Here are my tech skills</h1>
             <h1>Here are the links to my github and linkedin</h1>
             <h1>Here are my featured projects</h1>
