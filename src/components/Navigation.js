d8c0f1c8763e24b97541beb301f3c62010d5a740
Amber Taveras <taveras.amber@gmail.com>
2023-05-09 19:01:37 -0400

    adds media query hamburger menu for mobile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,35 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+
+import { MdClose } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
 
 function Navigation () {
+
+    const [navOpen, setNavOpen] = useState(false);
 
     return (
         <nav class='border-pink-200 bg-pink-100'>
             <div class='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-6'>
                 <span class='text-2xl font-semibold hover:font-bold'>Amber Taveras</span>
-            <div class='menu hidden w-full md:block md:w-auto'>
-            <ul class='flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent'>
-                <Link to='/'>
-                    <li class='hover:font-semibold'>About</li>
+                <button class='hamburger' onClick={() => setNavOpen((prev) => !prev)}>
+                {
+                    navOpen ? (<MdClose style={{ width: '32px', height: '28px'}} />)
+                        : (<FiMenu style={{width: '32px', height: '28px'}} />)
+                }
+                </button>
+
+            <div class={navOpen ? 'w-full text-right mr-8 ml-auto' : 'hidden w-full md:block md:w-auto'}>
+            <ul class= {navOpen ? 'flex flex-col pt-4 w-2/5 ml-auto' : 'flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent'} >
+                <Link to='/' onClick={() => setNavOpen(false)}>
+                    <li class='hover:font-semibold w-10/12'>About</li>
                 </Link>
-                <Link to='/projects'>
-                    <li class='hover:font-semibold'>Projects</li>
+                <Link to='/projects' onClick={() => setNavOpen(false)}>
+                    <li class='hover:font-semibold w-10/12'>Projects</li>
                 </Link>
-                <Link to='/resume'>
-                    <li class='hover:font-semibold'>Resume</li>
+                <Link to='/resume' onClick={() => setNavOpen(false)}>
+                    <li class='hover:font-semibold w-10/12'>Resume</li>
                 </Link>
             </ul>
             </div>
