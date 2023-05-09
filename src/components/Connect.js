08e35764faed3cd1be1bacbdfc58af641b4d64b0
Amber Taveras <taveras.amber@gmail.com>
2023-05-09 19:42:21 -0400

    styles footer to bottom and centers social links on page

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,10 +1,10 @@
 function Connect () {
     return (
-        <ul>
-            <li>
+        <ul class='flex justify-center'>
+            <li class='mx-3'>
                 <a href="https://github.com/ambertav/" target="_blank">Github</a>
             </li>
-            <li>
+            <li class='mx-3'>
                 <a href="https://www.linkedin.com/in/ambertaveras/" target="_blank">Linkedin</a>
             </li>
         </ul>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ function Footer () {
     const year = new Date().getFullYear()
 
     return (
-    <footer>
-        <div>
-            <span>Copyright &copy; {year} All Rights Reserved</span>
-        </div>
+    <footer class='relative bottom-0 w-full py-6 bg-gray-100 justify-center'>
         <Connect />
+        <div>
+            <p class='pt-6'>Copyright &copy; {year} All Rights Reserved</p>
+        </div>
     </footer>
     )
 }
