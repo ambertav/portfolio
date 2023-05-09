2808d581bb4041ae0d7a1a9bcc1e8ce468b2e91a
Amber Taveras <taveras.amber@gmail.com>
2023-05-09 19:49:46 -0400

    refactors routing to add home page at root

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router';
 
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
+import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Resume from './pages/Resume';
 import About from './pages/About';
@@ -15,7 +16,8 @@ function App() {
     <div className="App">
       <Navigation />
       <Routes>
-          <Route path='/' element={<About data={data} />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About data={data} />} />
           <Route path='/projects' element={<Projects projects={data.projects} />} />
           <Route path='/resume' element={<Resume />} />
       </Routes>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,7 +12,9 @@ function Navigation () {
     return (
         <nav class='border-pink-400 shadow bg-pink-200 h-20'>
             <div class='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-6'>
-                <span class='text-2xl font-semibold hover:font-bold'>Amber Taveras</span>
+                <Link to='/' onClick={() => setNavOpen(false)}>
+                    <span class='text-2xl font-semibold hover:font-bold'>Amber Taveras</span>
+                </Link>
                 <button class='hamburger' onClick={() => setNavOpen((prev) => !prev)}>
                 {
                     navOpen ? (<MdClose style={{ width: '32px', height: '28px'}} />)
@@ -21,7 +23,7 @@ function Navigation () {
                 </button>
                 <div class={navOpen ? 'w-full mr-10 md:hidden overflow-hidden' : 'hidden w-full md:block md:w-auto'}>
                     <ul class= {navOpen ? 'flex flex-col p-2 w-2/5 ml-auto shadow-2xl bg-white text-center rounded-lg' : 'flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent'} >
-                        <Link to='/' onClick={() => setNavOpen(false)}>
+                        <Link to='/about' onClick={() => setNavOpen(false)}>
                             <li class='hover:font-semibold'>About</li>
                         </Link>
                         <Link to='/projects' onClick={() => setNavOpen(false)}>
