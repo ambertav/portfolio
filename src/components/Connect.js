5391165c326418102c3db53fe4012979d5219805
Amber Taveras <taveras.amber@gmail.com>
2023-05-11 17:01:45 -0400

    adds social icons

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -2,10 +2,10 @@ function Connect () {
     return (
         <ul class='flex justify-center'>
             <li class='mx-3'>
-                <a href="https://github.com/ambertav/" target="_blank">Github</a>
+                <a href="https://github.com/ambertav/" target="_blank" rel="noreferrer"><img src={require('../icons/github.png')} alt="github" class='icons' /></a>
             </li>
             <li class='mx-3'>
-                <a href="https://www.linkedin.com/in/ambertaveras/" target="_blank">Linkedin</a>
+                <a href="https://www.linkedin.com/in/ambertaveras/" target="_blank" rel="noreferrer"><img src={require('../icons/linkedin.png')} alt="linkedin" class='icons' /></a>
             </li>
         </ul>
     );
