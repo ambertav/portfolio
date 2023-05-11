b7df3636fba49eeb61eb24a76b91fd513bbc3920
Amber Taveras <taveras.amber@gmail.com>
2023-05-11 15:01:07 -0400

    fixes sticky footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,11 @@ function Footer () {
     const year = new Date().getFullYear()
 
     return (
-    <footer class='relative bottom-0 w-full py-6 bg-gray-100 justify-center'>
+    <footer class='w-full py-6 bg-gray-200 justify-center'>
         <Connect />
         <div>
             <p class='pt-6'>Copyright &copy; {year} All Rights Reserved</p>
+            <p>Amber Taveras</p>
         </div>
     </footer>
     )
