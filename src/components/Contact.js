97aa190cddb03856e4360c560f37f22235a981fe
Amber Taveras <taveras.amber@gmail.com>
2023-05-13 11:51:06 -0400

    sets form state and assigns values to form inputs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,45 @@
-import { Form } from "react-router-dom";
+import { useState } from 'react';
 
-function Contact () {
+function Contact (props) {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const { name, email, message } = formState;
+
     return (
         <>
-        <h3>Contact</h3>
-        <form>
-            <label>
-                Name:
-                <input type="text" name="name" />
-            </label>
-            <label>
-                Email:
-                <input type="text" name="email" />
-            </label>
-            <label>
-                Message
-                <input type="text" name="message" />
-            </label>
-            <input type="submit" value="Send Message" />
-        </form>
+            <h3>Contact</h3>
+            <form>
+                <label>
+                    Name:
+                    <input 
+                        type="text"
+                        name="name"
+                        value={name}
+                    />
+                </label>
+                <label>
+                    Email:
+                    <input
+                        type="text"
+                        name="email"
+                        value={email}
+                    />
+                </label>
+                <label>
+                    Message:
+                    <input
+                        type="text"
+                        name="message"
+                        value={message}
+                    />
+                </label>
+                <input type="submit" value="Send Message" />
+            </form>
         </>
     );
 }
