553add4a8630a8bf5d68a57db4b005b6e969854d
Amber Taveras <taveras.amber@gmail.com>
2023-05-13 11:46:26 -0400

    renders contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,25 @@
+import { Form } from "react-router-dom";
+
 function Contact () {
     return (
-        <h1>this is the contact form</h1>
+        <>
+        <h3>Contact</h3>
+        <form>
+            <label>
+                Name:
+                <input type="text" name="name" />
+            </label>
+            <label>
+                Email:
+                <input type="text" name="email" />
+            </label>
+            <label>
+                Message
+                <input type="text" name="message" />
+            </label>
+            <input type="submit" value="Send Message" />
+        </form>
+        </>
     );
 }
 
