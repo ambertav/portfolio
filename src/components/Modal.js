18972bf07ff5fd7cb6b117099aabf7b45f2212c8
Amber Taveras <taveras.amber@gmail.com>
2023-05-17 18:13:47 -0400

    adds thank message, handle click to close

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,18 @@
-function Modal () {
+function Modal (props) {
+
+    function handleClick (evt) {
+        evt.preventDefault();
+        props.setModalOpen(false);
+    }
+
     return (
-        <h1>this is the modal</h1>
+        <div className='modal'>
+            <h1>Thank you for submitting!</h1>
+            <p>I'll get in contact with you as soon as possible</p>
+            <button className='btn-secondary px-5' onClick={handleClick}>
+                Close
+            </button>
+        </div>
     );
 }
 
