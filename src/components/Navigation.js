432418476066cf563384eca0ae4dfcae1e11e4d9
Amber Taveras <taveras.amber@gmail.com>
2023-05-20 03:23:06 -0400

    cleans up evt handler when comp unmounts, uses useref for menu el

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,28 +1,36 @@
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { HashLink } from 'react-router-hash-link';
 
 function Navigation () {
 
     const [navOpen, setNavOpen] = useState(false);
 
-    function scrollCenter (el) {
-        el.scrollIntoView({
+    const menuRef = useRef(null);
+    
+    function scrollCenter (element) {
+        element.scrollIntoView({
             block: 'center'
         });
     }
 
     function clickedOutsideOfNav (evt) {
         if (evt.target === document.querySelector('.menu-icon')) return;
-        const menu = document.querySelector('#menu')
-        const itemsArray = Array.from(menu.querySelectorAll('li'));
+        const itemsArray = Array.from(menuRef.current.querySelectorAll('li'));
         if (!itemsArray.includes(evt.target)) {
             setNavOpen(false);
         }
     }
 
-    useEffect(function () {
+    function handleNavHamburger () {
+        setNavOpen((prev) => !prev);
+    }
+
+    useEffect(() => {
         document.addEventListener('click', clickedOutsideOfNav);
+        return () => {
+            document.removeEventListener('click', clickedOutsideOfNav);
+        };
     }, []);
 
     return (
@@ -31,14 +39,14 @@ function Navigation () {
                 <Link to='/' onClick={() => setNavOpen(false)}>
                     <span className='text-2xl font-semibold hover:font-bold'>Amber Taveras</span>
                 </Link>
-                <button className='hamburger' onClick={() => setNavOpen((prev) => !prev)}>
+                <button className='hamburger' onClick={handleNavHamburger}>
                 {
                     navOpen ? (<img className='menu-icon' src={require('../icons/close.png')} alt="x-button icon to close menu" style={{ width: '32px', height: '32px'}} />)
                         : (<img className='menu-icon' src={require('../icons/hamburger.png')} alt="hamburger icon to open menu" style={{width: '32px', height: '32px'}} />)
                 }
                 </button>
                 <div className={navOpen ? 'w-full mr-10 ml-auto md:hidden overflow-visible' : 'hidden w-full md:block md:w-auto'}>
-                    <ul id='menu' className= {navOpen ? 'flex flex-col p-2 w-2/6 ml-auto shadow-2xl bg-white text-center rounded-lg absolute right-12 [&>*]:p-2'
+                    <ul id='menu' ref={menuRef} className= {navOpen ? 'flex flex-col p-2 w-2/6 ml-auto shadow-2xl bg-white text-center rounded-lg absolute right-12 [&>*]:p-2'
                     : 'flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent'} >
                         <HashLink
                             to='/#bio'
