8bc3aeb890c848638bf9c18fa99a98107f5ecd7b
Amber Taveras <taveras.amber@gmail.com>
2023-06-11 01:15:20 -0400

    uses state to render tooltip content specific to icon

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,14 @@
+import { useState } from 'react';
 import { Tooltip } from 'react-tooltip';
 
 function Skills (props) {
+
+    const [ tooltipContent, settooltipContent ] = useState('');
+
+    function handleMouseOver (evt) {
+        settooltipContent(`${evt.target.alt.replace('-', '.')}`);
+    }
+
     return (
         <div className='flex justify-center items-center pt-3 mb-6'>
             <ul className='flex justify-center items-center flex-wrap w-3/5'>
@@ -8,9 +16,9 @@ function Skills (props) {
                     return (
                         <li className='p-3' key={index}>
                             <Tooltip anchorSelect='.my-anchor-element' place="top">
-                                    {s}
+                                    {tooltipContent}
                             </Tooltip>
-                            <img src={require(`../icons/${s.toLowerCase()}.png`)} alt={s} className='icons my-anchor-element' />
+                            <img onMouseOver={handleMouseOver} src={require(`../icons/${s.toLowerCase()}.png`)} alt={s} className='icons my-anchor-element' />
                         </li>
                     );
                 })}
