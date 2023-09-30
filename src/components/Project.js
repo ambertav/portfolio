a6145db47d36191d6692136f019db24b6989609d
Amber Taveras <taveras.amber@gmail.com>
2023-09-30 04:01:01 -0400

    refactors for index and show page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,23 +1,19 @@
 function Project (props) {
     return (
         <div className='flex flex-wrap justify-evenly items-start'>
-            {props.projects.map((p, index) => {
-                    return (
-                        <div className='project-card m-10 md:m-20' key={index}>
-                            <img src={require(`../project-media/${p.src}.png`)} alt={p.name}/>
-                            <h4 className='p-3 mt-4 mb-2 font-semibold text-lg'>{p.name}</h4>
-                            <a className='btn-secondary' href={p.deployLink} target="_blank" rel="noreferrer">View Deployment</a>
-                            <a className='btn-secondary' href={p.repoLink} target="_blank" rel="noreferrer">View Repository</a>
-                            <p className='text-xs py-4 my-2 md:text-lg'>{p.description}</p>
+                        <div className='project-card m-10 md:m-20'>
+                            <img src={require(`../project-media/${props.project.src}.png`)} alt={props.project.name}/>
+                            <h4 className='p-3 mt-4 mb-2 font-semibold text-lg'>{props.project.name}</h4>
+                            <a className='btn-secondary' href={props.project.deployLink} target="_blank" rel="noreferrer">View Deployment</a>
+                            <a className='btn-secondary' href={props.project.repoLink} target="_blank" rel="noreferrer">View Repository</a>
+                            <p className='text-xs py-4 my-2 md:text-lg'>{props.project.description}</p>
                             <ul className='stack'>
                                 <h4 className='font-semibold'>Technologies Used:</h4>
-                                {p.stack.map((tech, index) => {
+                                {props.project.stack.map((tech, index) => {
                                     return <li key={index} className='px-4'>{tech}</li>
                                 })}
                             </ul>
                         </div>
-                    );
-                })}
         </div>
     );
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Project from '../components/Project';
 import { useEffect } from 'react';
 
@@ -10,12 +11,23 @@ function Projects (props) {
 
     return (
         <>
-            <section className='border-b-2 flex flex-start flex-col text-left'>
-                <h1 className='mt-10 md:ml-10 ml-6 font-semibold text-2xl p-3'>Projects</h1>
-                <h5 className='md:ml-10 ml-6 py-4 px-3 text-xs md:text-sm'>View this portfolio's repository <a href="https://github.com/ambertav/portfolio" target="_blank" rel="noreferrer" className='text-red-900 font-bold'>here</a></h5>
+            <section className='border-b-2 text-left mx-10 lg:mx-16'>
+                <h1 className='mt-10 md:ml-10 ml-6 font-semibold text-3xl p-3'>Projects</h1>
             </section>
             <div>
-                <Project projects={props.projects}/>
+                <ul className='flex flex-wrap items-center justify-evenly'>
+                {props.projects.map((p) => {
+                    return (
+                        <li className='m-6 w-4/5 lg:w-2/5 lg:m-10 rounded-lg shadow-2xl'>
+                                <Link to={`/projects/${p.name.toLowerCase().replaceAll(' ', '-')}`} state={{ project: p }}>
+                                    <img src={require(`../project-media/${p.src}.png`)} alt={p.name} className='rounded-lg shadow-2xl w-full h-full'/>
+                                </Link>
+                            </li>
+                    )
+                })}
+                </ul>
+
+                {/* <Project projects={props.projects}/> */}
             </div>
         </>
     );
