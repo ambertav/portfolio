f76397909791a9d926f26309c872ec77a6129fbf
Amber Taveras <taveras.amber@gmail.com>
2023-09-30 02:06:27 -0400

    updates skills section with new styles

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,14 +11,17 @@ function Skills (props) {
 
     return (
         <div className='flex justify-center items-center pt-6 mb-12'>
-            <ul className='flex justify-center items-center flex-wrap w-3/5'>
+            <ul className='flex justify-evenly flex-wrap w-full'>
                 {props.skills.map((s, index) => {
                     return (
-                        <li className='p-3' key={index}>
+                        <li className='p-6 m-4 mx-10 w-full flex md:justify-evenly items-center shadow-2xl text-xl font-bold md:w-1/3 lg:w-1/4 transition delay-200 duration-500 ease-in-out hover:bg-pink-100 hover:text-red-400' key={index}>
                             <Tooltip anchorSelect='.my-anchor-element' place="top">
                                     {tooltipContent}
                             </Tooltip>
-                            <img onMouseOver={handleMouseOver} src={require(`../icons/${s.toLowerCase()}.png`)} alt={s.replace('-', '.')} className='icons my-anchor-element' />
+                            <img onMouseOver={handleMouseOver} src={require(`../icons/${s.toLowerCase()}.png`)} alt={s.replace('-', '.')} className='icons my-anchor-element mr-16 md:mr-8' />
+                            <li>
+                                {s.replace('-', '.')}
+                            </li>
                         </li>
                     );
                 })}
