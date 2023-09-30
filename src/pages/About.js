8fbd66d77cf63d7b21af3607565ac9af8690ea9e
Amber Taveras <taveras.amber@gmail.com>
2023-09-30 03:15:29 -0400

    re renders connect component

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 
 import Header from '../components/Header';
 import Skills from '../components/Skills';
+import Connect from '../components/Connect';
 
 function About (props) {
 
@@ -13,8 +14,10 @@ function About (props) {
     return (
         <div>
             <Header bio={props.data.bio} />
-            <h3 className='font-semibold text-2xl mt-12' id='skills'>Technical Skills</h3>
+            <h3 className='font-semibold text-2xl md:text-3xl mt-20' id='skills'>Technical Skills</h3>
             <Skills skills={props.data.skills} />
+            <h3 className='font-semibold text-2xl md:text-3xl mt-20 border-t-2 mx-10 lg:mx-16 pt-10' id='connect'>Connect with Amber</h3>
+            <Connect />
         </div>
     );
 }
