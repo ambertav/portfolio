8bf9c99197e27dd020b15f76ad400fde0c876986
Amber Taveras <taveras.amber@gmail.com>
2023-10-02 01:56:29 -0400

    adds image cover on hover to link to project repo/deploy

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import Project from '../components/Project';
 import { useEffect } from 'react';
 
 function Projects (props) {
@@ -13,21 +12,28 @@ function Projects (props) {
         <>
             <section className='border-b-2 text-left mx-10 lg:mx-16'>
                 <h1 className='mt-10 md:ml-10 ml-6 font-semibold text-3xl p-3'>Projects</h1>
+
             </section>
             <div>
                 <ul className='flex flex-wrap items-center justify-evenly'>
                 {props.projects.map((p) => {
                     return (
                         <li className='m-6 w-4/5 lg:w-2/5 lg:m-10 rounded-lg shadow-2xl'>
-                                <Link to={`/projects/${p.name.toLowerCase().replaceAll(' ', '-')}`} state={{ project: p }}>
-                                    <img src={require(`../project-media/${p.src}.png`)} alt={p.name} className='rounded-lg shadow-2xl w-full h-full'/>
-                                </Link>
-                            </li>
-                    )
+                            <Link to={`/projects/${p.name.toLowerCase().replaceAll(' ', '-')}`} state={{ project: p }}>
+                                <div className='relative group'>
+                                    <img src={require(`../project-media/${p.src}.png`)} alt={p.name} className='rounded-lg shadow-2xl w-full h-full' />
+                                    <div className='absolute inset-0 bg-stone-800 bg-opacity-0 group-hover:bg-opacity-80 transition-opacity'>
+                                        <h4 className='p-3 md:mt-16 md:mb-12 mb-6 font-semibold text-xl md:text-2xl text-white opacity-0 group-hover:opacity-100 transition-opacity'>{p.name}</h4>
+                                        <a className='btn-secondary opacity-0 group-hover:opacity-100 transition-opacity md:mx-4 p-3' href={p.deployLink} target="_blank" rel="noreferrer">View Deployment</a>
+                                        <a className='btn-secondary opacity-0 group-hover:opacity-100 transition-opacity md:mx-4 p-3' href={p.repoLink} target="_blank" rel="noreferrer">View Repository</a>
+                                        <p className='text-sm opacity-0 group-hover:opacity-100 transition-opacity md:my-20 my-6 text-white'>Click for more details</p>
+                                    </div>
+                                </div>
+                            </Link>
+                        </li>
+                    );
                 })}
                 </ul>
-
-                {/* <Project projects={props.projects}/> */}
             </div>
         </>
     );
