a0e485ff6a7fa18b9dc5ac2d355a84f3c335d86c
Amber Taveras <taveras.amber@gmail.com>
2023-11-06 22:31:00 -0500

    displays skills by category

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,23 +10,16 @@ function Skills (props) {
     }
 
     return (
-        <div className='flex justify-center items-center pt-6 mb-12'>
-            <ul className='flex justify-evenly flex-wrap w-full'>
-                {props.skills.map((s, index) => {
-                    return (
-                        <li className='p-6 m-4 mx-10 w-full flex md:justify-evenly items-center rounded-md shadow-lg text-xl font-bold md:w-1/3 lg:w-1/4 transition delay-200 duration-500 ease-in-out hover:bg-pink-100 hover:text-red-400' key={index}>
-                            <Tooltip anchorSelect='.my-anchor-element' place="top">
-                                    {tooltipContent}
-                            </Tooltip>
-                            <img onMouseOver={handleMouseOver} src={require(`../icons/${s.toLowerCase()}.png`)} alt={s.replace('-', '.')} className='icons my-anchor-element mr-16 md:mr-8' />
-                            <li>
-                                {s.replace('-', '.')}
-                            </li>
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
+        <li className='p-4 mx-4 mb-4 md:my-8 w-full flex md:justify-evenly items-center rounded-md shadow-lg text-lg font-bold md:w-1/3 lg:w-1/4 transition delay-200 duration-500 ease-in-out hover:bg-pink-100 hover:text-red-400'>
+            <Tooltip anchorSelect='.anchorElement' place="top">{tooltipContent}</Tooltip>
+            <img 
+                onMouseOver={handleMouseOver} 
+                onMouseOut={() => settooltipContent('')} 
+                src={require(`../assets/icons/${props.skill.toLowerCase()}.png`)} 
+                alt={props.skill.replace('-', '.')} className='icons anchorElement mr-10 md:mr-4' 
+            />
+            <li>{props.skill.replace('-', '.')}</li>
+        </li>
     );
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -14,9 +14,18 @@ function About (props) {
     return (
         <div>
             <Header bio={props.data.bio} />
-            <h3 className='font-semibold text-2xl md:text-3xl mt-14 mb-4' id='skills'>Technical Skills</h3>
-            <Skills skills={props.data.skills} />
-            <h3 className='font-semibold text-2xl md:text-3xl mt-20 border-t-2 mx-10 lg:mx-16 pt-10 mb-4' id='connect'>Connect with Amber</h3>
+            <h3 className='font-semibold text-3xl md:text-4xl mt-14 mb-4'>Technical Skills</h3>
+            <div className='flex flex-col justify-center items-center'>
+                {Object.keys(props.data.skills).map((category) => (
+                <div className='w-4/5' key={category}>
+                    <h2 className='capitalize font-semibold text-lg md:text-2xl text-left mb-4 md:mb-2 mt-6 text-zinc-400'>{category}</h2>
+                    <ul className='flex flex-row flex-wrap justify-evenly w-full'>
+                    { props.data.skills[category].map((skill, index) => <Skills skill={skill} key={index} /> )}
+                    </ul>
+                </div>
+                ))}
+            </div>
+            <h3 className='font-semibold text-3xl md:text-4xl mt-20 border-t-2 mx-10 lg:mx-16 pt-10 mb-4' id='connect'>Connect with Amber</h3>
             <Connect />
         </div>
     );
